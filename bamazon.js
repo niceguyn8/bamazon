47ee97f27e0a29214da6fb6bfd19febbcf66ff38
Nathan Schubert <schubert.nathan@gmail.com>
2018-05-04 22:21:46 -0400

    table generates in command line

--- bamazon.js
+++ bamazon.js
@@ -12,4 +12,15 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected!");
+  table();
 });
+
+var table = function() {
+  connection.query("SELECT * FROM products", function(err, res){
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].item_id+" || "+res[i].product_name+" || "+
+      res[i].department_name+" || "+res[i].price+" || "+res[i].
+      stock_quantity+"\n");
+    }
+  })
+}
